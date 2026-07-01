98f2a0455c4cc4e8674d333c4adabf4a619ed04f
prosen015 <sarkarprosenjit0153@gmail.com>
2026-07-01 20:53:01 +0530

    Initialize config.py with bot settings
    Add configuration for bot token, download directory, and start message.

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+BOT_TOKEN = os.getenv("8982070912:AAEoD798WuQfXSgKU-cFuZH0Q5dIkmpnlvg")
+
+DOWNLOAD_DIR = "downloads"
+
+START_MESSAGE = """
+👋 Welcome!
+
+📥 Send me any Public Instagram Reel or Post link.
+
+✅ I will download it and send it back to you.
+
+Examples:
+https://www.instagram.com/reel/......
+https://www.instagram.com/p/......
+"""
